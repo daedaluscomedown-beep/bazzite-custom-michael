8f9dc07f6e12e0dcaad08f92c510f326a39bb5f4
daedaluscomedown-beep <daedaluscomedown@gmail.com>
2026-01-26 16:43:50 -0500

    Nuclear fix for GPG key

--- scripts/tweaks.sh
+++ scripts/tweaks.sh
@@ -7,10 +7,10 @@ update-mime-database /usr/share/mime
 # 2. Enable the Undervolt Service for All Users
 systemctl --global enable undervolt.service
 
-# 3. FIX: Patch broken Terra Repository GPG Key
-# The build failed because 'terra-mesa' points to a missing local key.
-# We will force-disable the GPG check for this repo so the ISO build can finish.
-if grep -r "RPM-GPG-KEY-terra43-mesa" /etc/yum.repos.d/; then
-    echo "Found broken Terra key reference. Disabling GPG check..."
-    grep -l "RPM-GPG-KEY-terra43-mesa" /etc/yum.repos.d/*.repo | xargs sed -i 's/gpgcheck=1/gpgcheck=0/g'
-fi
+# 3. FIX: NUCLEAR OPTION for Terra Repository
+# We are deleting the line that points to the missing key entirely.
+echo "Nuking broken Terra key reference..."
+find /etc/yum.repos.d/ -name "*.repo" -print0 | xargs -0 sed -i '/gpgkey=.*RPM-GPG-KEY-terra43-mesa/d'
+
+# 4. Double Tap: Force GPG Check OFF for that repo just in case
+find /etc/yum.repos.d/ -name "*.repo" -print0 | xargs -0 sed -i '/\[terra-mesa\]/,/^$/s/gpgcheck=1/gpgcheck=0/'
