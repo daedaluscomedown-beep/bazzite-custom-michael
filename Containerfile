c0cee6bef5ac46c27a7f97bfa2e6d01ae5c8c9d1
daedaluscomedown-beep <daedaluscomedown@gmail.com>
2026-01-29 18:02:50 -0500

    Spectrum Core

--- Containerfile
+++ Containerfile
@@ -1,40 +1,18 @@
-# Allow build scripts to be referenced without being copied into the final image
-FROM scratch AS ctx
-COPY build_files /
+# We use the base image defined in your recipe
+FROM ghcr.io/ublue-os/bazzite-kde:stable
 
-# Base Image
-FROM ghcr.io/ublue-os/bazzite:stable
+# 1. Copy your custom files (overlays files/usr -> /usr, files/etc -> /etc)
+# This ensures your EDID firmware and undervolt service units are in place.
+COPY files /
 
-## Other possible base images include:
-# FROM ghcr.io/ublue-os/bazzite:latest
-# FROM ghcr.io/ublue-os/bluefin-nvidia:stable
-# 
-# ... and so on, here are more base images
-# Universal Blue Images: https://github.com/orgs/ublue-os/packages
-# Fedora base image: quay.io/fedora/fedora-bootc:41
-# CentOS base images: quay.io/centos-bootc/centos-bootc:stream10
+# 2. Copy your scripts so build.sh can run them
+COPY scripts /tmp/scripts
 
-### [IM]MUTABLE /opt
-## Some bootable images, like Fedora, have /opt symlinked to /var/opt, in order to
-## make it mutable/writable for users. However, some packages write files to this directory,
-## thus its contents might be wiped out when bootc deploys an image, making it troublesome for
-## some packages. Eg, google-chrome, docker-desktop.
-##
-## Uncomment the following line if one desires to make /opt immutable and be able to be used
-## by the package manager.
+# 3. Copy the main build script
+COPY build_files/build.sh /tmp/build.sh
 
-# RUN rm /opt && mkdir /opt
+# 4. Run the build script
+RUN chmod +x /tmp/build.sh && /tmp/build.sh
 
-### MODIFICATIONS
-## make modifications desired in your image and install packages by modifying the build.sh script
-## the following RUN directive does all the things required to run "build.sh" as recommended.
-
-RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
-    --mount=type=cache,dst=/var/cache \
-    --mount=type=cache,dst=/var/log \
-    --mount=type=tmpfs,dst=/tmp \
-    /ctx/build.sh
-    
-### LINTING
-## Verify final image and contents are correct.
-RUN bootc container lint
+# 5. Cleanup temporary scripts
+RUN rm -rf /tmp/scripts /tmp/build.sh
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,65 @@
 
 set -ouex pipefail
 
-### Install packages
+echo "--- STARTING CUSTOM BUILD ---"
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+# ==============================================================================
+# 1. DISABLE TERRA REPOS (From disable-terra.sh)
+# ==============================================================================
+echo "--- Disabling Terra Repositories ---"
+# This prevents conflicts during the build process
+for repo_file in $(find /etc/yum.repos.d /usr/etc/yum.repos.d -name "*terra*.repo" 2>/dev/null); do
+    echo "Disabling repo: $repo_file"
+    sed -i 's/enabled=1/enabled=0/g' "$repo_file"
+done
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# ==============================================================================
+# 2. INSTALL PACKAGES (From recipe.yml modules)
+# ==============================================================================
+echo "--- Installing RPM Packages ---"
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# Define your packages
+PACKAGES=(
+    "ryzenadj"
+    "alsa-firmware"
+    "lact"
+)
 
-#### Example for enabling a System Unit File
+# Install them
+rpm-ostree install "${PACKAGES[@]}"
 
-systemctl enable podman.socket
+# ==============================================================================
+# 3. APPLY KERNEL ARGUMENTS (From kargs.sh)
+# ==============================================================================
+echo "--- Applying Kernel Arguments via Config File ---"
+
+# instead of running rpm-ostree kargs, we write a static config file.
+# This is the stable, "bootc" compliant way to do it.
+
+KARGS_FILE="/usr/lib/kernel/cmdline.d/99-michael-custom.conf"
+mkdir -p "$(dirname "$KARGS_FILE")"
+
+cat <<EOF > "$KARGS_FILE"
+# Michael's Custom Kargs
+split_lock_detect=off
+amdgpu.ppfeaturemask=0xffffffff
+drm.edid_firmware=DP-3:edid/samsung_g5_custom.bin
+video=DP-3:e
+EOF
+
+echo "Created $KARGS_FILE"
+
+# ==============================================================================
+# 4. SYSTEM TWEAKS (From tweaks.sh)
+# ==============================================================================
+echo "--- Applying Final System Tweaks ---"
+
+# Ensure MIME database sees Chrome as default
+if [ -d "/usr/share/mime" ]; then
+    update-mime-database /usr/share/mime
+fi
+
+# Enable the Undervolt Service (Assumes unit file was copied via 'files/etc/systemd/system')
+systemctl --global enable undervolt.service
+
+echo "--- BUILD COMPLETE ---"
